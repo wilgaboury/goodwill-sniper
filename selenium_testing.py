dac3709d0a8e4e6811d6a15598dd8dedd558dcb2
wilgaboury <wgaboury@gmail.com>
2021-02-04 00:46:10 -0500

    changes

--- selenium_testing.py
+++ selenium_testing.py
@@ -1,7 +1,12 @@
 import sqlite3
 import json
 import datetime
-import time
+import pause
+import utils
+import math
+import socket
+from dateutil.parser import parse
+from multiprocessing.connection import Listener
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
@@ -13,24 +18,40 @@ from apscheduler.schedulers.background import BackgroundScheduler
 init_config = json.loads(open('config.json', 'r').read())
 
 item_id = '114119260'
+max_bid = 10
 
-driver = Firefox()
-driver.get('https://www.shopgoodwill.com/SignIn')
+try:
+    driver = Firefox()
+    driver.get('https://www.shopgoodwill.com/SignIn')
 
-username_input = driver.find_element_by_id('Username')
-username_input.send_keys(init_config['username'])
-password_input = driver.find_element_by_id('Password')
-password_input.send_keys(init_config['password'])
-driver.find_element_by_id('login-submit').click()
+    username_input = driver.find_element_by_id('Username')
+    username_input.send_keys(init_config['username'])
+    password_input = driver.find_element_by_id('Password')
+    password_input.send_keys(init_config['password'])
+    driver.find_element_by_id('login-submit').click()
 
-driver.get('https://www.shopgoodwill.com/Item/' + str(item_id))
-minimum_bid = float(driver.find_element_by_css_selector('.minimum-bid').get_attribute('innerHTML')[1:])
-bid_amount = minimum_bid + init_config['added_to_bid']
+    driver.get('https://www.shopgoodwill.com/Item/' + str(item_id))
 
-driver.find_element_by_css_selector('.cc-btn.cc-dismiss').click()
+    driver.find_element_by_css_selector('.cc-btn.cc-dismiss').click()
 
+    minimum_bid = float(driver.find_element_by_css_selector('.minimum-bid').get_attribute('innerHTML')[1:])
+    bid_amount = math.ceil(minimum_bid)
 
-bid_input = driver.find_element_by_id('bidAmount')
-bid_input.send_keys('{:.2f}'.format(round(bid_amount, 2)))
-driver.find_element_by_id('placeBid').click()
-driver.find_element_by_id('place-bid-modal').click()
+    while bid_amount <= max_bid:
+        bid_input = driver.find_element_by_id('bidAmount')
+        bid_input.send_keys('{:.2f}'.format(round(bid_amount, 2)))
+
+        driver.find_element_by_id('placeBid').click()
+        driver.find_element_by_id('place-bid-modal').click()
+
+        if driver.find_element_by_id('bid-result').get_attribute('innerHTML').find('You have already been outbid.') >= 0:
+            break
+        else:
+            driver.find_element_by_css_selector('.modal-footer button.btn.btn-default').click()
+            bid_amount += 1
+
+except Exception as e:
+    print("Sniping item #" + str(item_id) + " failed")
+    print(e)
+else:
+    print("sniped item #" + str(item_id))
